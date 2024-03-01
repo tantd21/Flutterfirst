d55cc27b915860036852b3f9f844d5f7be76cbaf
tantd21 <ta090707@gmail.com>
2024-03-01 17:31:15 +0700

    MongoDB

--- lib/dbHelper/constant.dart
+++ lib/dbHelper/constant.dart
@@ -1,5 +1,7 @@
-const MONGO_CONN_URL = "mongodb+srv://demo_user:<vv2bGjxbsmZssJSO>@cluster0.ahwk4vu.mongodb.net/firsttime?retryWrites=true";
-const USER_COLLECTION = "firsttime.fitest";
+// const MONGO_CONN_URL = "mongodb+srv://demo_user:<vv2bGjxbsmZssJSO>@cluster0.ahwk4vu.mongodb.net/firsttime?retryWrites=true";
+const USER_COLLECTION = "fitest";
+const MONGO_CONN_URL = "mongodb+srv://demo_user:XLmi7ZJlUcUzgNJL@cluster0.ahwk4vu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // user = "demo_user"
 // password = "vv2bGjxbsmZssJSO"
+// mongodb+srv://<demo_user>:<Psy4R79NzS6D3Vwa>@cluster0.ahwk4vu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
